206669ef62c1881912ca1a8780f37544fcfac614
NOEL PAUL GEORGE <75027970+Noel6161131110@users.noreply.github.com>
2023-02-01 01:01:42 +0530

    second bubble sort commit

--- Sorting/Bubble Sort/BubbleSort.cpp
+++ Sorting/Bubble Sort/BubbleSort.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 using namespace std;
 
+void read(int a[10], int n)
+{
+    cout << "Enter the elements: \n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+}
+
+void swap(int* a, int* b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int a[10], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                swap(&a[j], &a[j + 1]);
+            }
+        }
+    }
+}
+
+void print(int a[10], int n)
+{
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+}
+
 int main()
 {
     int a[10], n;
@@ -8,31 +48,11 @@ int main()
     cout << "Enter size of the array: ";
     cin >> n;
 
-    cout << "Enter the elements: \n";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+    read(a, n);
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (a[j] > a[j + 1])
-            {
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
-            }
-        }
-    }
+    bubbleSort(a, n);
 
-    cout << endl;
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << a[i] << endl;
-    }
+    print(a, n);
 
     return 0;
 }
