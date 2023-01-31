07d0058a1bed60cd6bd62144dbe4e256b198e5c7
NOEL PAUL GEORGE <75027970+Noel6161131110@users.noreply.github.com>
2023-01-31 23:53:08 +0530

    bubble sort commit

---
+++ Sorting/Bubble Sort/BubbleSort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a[10], n;
+
+    cout<<"Enter size of the array: ";
+    cin>>n;
+
+    cout<<"Enter the elements: \n";
+    for(int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - i -1; j++)
+        {
+            if(a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    cout<<endl;
+    
+    for(int i = 0; i < n; i++)
+    {
+        cout<<a[i]<<endl;
+    }
+
+
+    return 0;
+}
