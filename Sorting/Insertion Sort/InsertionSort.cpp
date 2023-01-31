1f4dbea90a17dd3e58a10a5a9d8887c80032ce4c
NOEL PAUL GEORGE <75027970+Noel6161131110@users.noreply.github.com>
2023-02-01 00:32:27 +0530

    insertion commit

--- Sorting/Insertion Sort/InsertionSort.cpp
+++ Sorting/Insertion Sort/InsertionSort.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int a[10], n;
 
-    cout<<"Enter size of the array: ";
-    cin>>n;
+    cout << "Enter size of the array: ";
+    cin >> n;
 
-    cout<<"Enter the elements: \n";
-    for(int i = 0; i < n; i++)
+    cout << "Enter the elements: \n";
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
 
-    for(int i = 1; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
         int j = i;
-        while((a[j] < a[j - 1]) && j > 0)
+        while ((a[j] < a[j - 1]) && j > 0)
         {
             int temp = a[j];
             a[j] = a[j - 1];
@@ -26,11 +26,11 @@ int main()
         }
     }
 
-    cout<<endl;
-    
-    for(int i = 0; i < n; i++)
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
     {
-        cout<<a[i]<<endl;
+        cout << a[i] << endl;
     }
 
     return 0;
